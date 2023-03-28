ac7a43e9860f654efd13b35e382aecb70ebc0c52
vscatine <vitor.scatine@gmail.com>
2023-03-27 21:58:42 -0300

    Condições I

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log('Você ultrapassou o limite de velocidade permitido. MULTADO!')
+} else {
+    console.log('Você está dentro do limite de velocidade permitido. Boa viagem!')
+}
---
+++ Aula11/ex009.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if (pais == 'Brasil') {
+    console.log(`Você é brasileiro.`)
+} else {
+    console.log('Você é estrangeiro.')
+}
